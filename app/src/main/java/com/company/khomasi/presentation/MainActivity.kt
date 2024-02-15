d770c01b2e5f9ab9b4f7579345a1cedeba670550
Zeyad Abdullah <zeyadgalal93@gmail.com>
2024-02-15 17:44:09 +0200

    add icon launcher and splash

--- app/src/main/java/com/company/khomasi/presentation/MainActivity.kt
+++ app/src/main/java/com/company/khomasi/presentation/MainActivity.kt
@@ -5,12 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.Text
 import androidx.compose.ui.res.stringResource
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.company.khomasi.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContent {
             Text(
