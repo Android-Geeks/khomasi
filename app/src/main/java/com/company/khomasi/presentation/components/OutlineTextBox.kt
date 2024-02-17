cc684a8e7183d889f72f5d665ec0fc99998d0937
Zeyad Abdullah <zeyadgalal93@gmail.com>
2024-02-17 07:49:24 +0200

    fix outlinedTextBox

--- app/src/main/java/com/company/khomasi/presentation/components/OutlineTextBox.kt
+++ app/src/main/java/com/company/khomasi/presentation/components/OutlineTextBox.kt
@@ -1,5 +1,7 @@
 package com.company.khomasi.presentation.components
 
+import android.content.res.Configuration.UI_MODE_NIGHT_NO
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -18,79 +20,112 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.runtime.InternalComposeApi
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import com.company.khomasi.R
 
 @Composable
-@InternalComposeApi
 fun TextField(
-    leadingIcon : Painter,
-    value : String ,
-    onValueChange : (String) -> Unit,
-    @StringRes text : Int,
-    placeholder: String?=null,
-    keyBoardType : KeyboardType ,
-    imAction : ImeAction = ImeAction.Done,
-    onDone : () -> Unit,
-    modifier : Modifier = Modifier
-
-    ) {
-    Column {
-        Text(text = stringResource(id = text), textAlign = TextAlign.Start)
-        Spacer(modifier = modifier.height(6.dp))
-        OutlinedTextField(value = value,
-            onValueChange =onValueChange,
-            singleLine = true,
-            leadingIcon =  {
-                Icon(painter = leadingIcon,
-                    contentDescription = null) },
-            modifier = Modifier
-                .width(358.dp)
-                .height(82.dp)
-                .padding(horizontal = 16.dp, vertical = 12.dp),
-            placeholder = {placeholder?.let { Text(text = placeholder)}},
-            keyboardOptions = KeyboardOptions(
-                keyboardType = keyBoardType,
-                imeAction = imAction),
-            keyboardActions = KeyboardActions(onDone = {onDone })
-
-        )
-    }
-
-}
-
-@Composable
-@InternalComposeApi
-fun TextFieldWithoutIcon(
-    value : String ,
-    onValueChange : (String) -> Unit,
-    @StringRes text : Int,
-    placeholder: String?=null,
-    keyBoardType : KeyboardType ,
-    imAction : ImeAction = ImeAction.Done,
-    onDone : () -> Unit,
-    modifier : Modifier = Modifier
-
+    leadingIcon: Painter,
+    value: String,
+    onValueChange: (String) -> Unit,
+    @StringRes text: Int,
+    onDone: () -> Unit,
+    keyBoardType: KeyboardType,
+    modifier: Modifier = Modifier,
+    placeholder: String? = null,
+    imAction: ImeAction = ImeAction.Done,
 ) {
     Column {
         Text(text = stringResource(id = text), textAlign = TextAlign.Start)
-        Spacer(modifier = modifier.height(6.dp))
-        OutlinedTextField(value = value,
-            onValueChange =onValueChange,
+        OutlinedTextField(
+            value = value,
+            onValueChange = onValueChange,
+            singleLine = true,
+            leadingIcon = {
+                Icon(
+                    painter = leadingIcon,
+                    contentDescription = null
+                )
+            },
+            modifier = Modifier
+                .width(358.dp)
+                .height(82.dp)
+                .padding(horizontal = 16.dp, vertical = 12.dp),
+            placeholder = { placeholder?.let { Text(text = placeholder) } },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = keyBoardType,
+                imeAction = imAction
+            ),
+            keyboardActions = KeyboardActions(onDone = { onDone() })
+        )
+    }
+
+}
+
+@Composable
+fun TextFieldWithoutIcon(
+    value: String,
+    onValueChange: (String) -> Unit,
+    @StringRes text: Int,
+    keyBoardType: KeyboardType,
+    onDone: () -> Unit,
+    modifier: Modifier = Modifier,
+    imAction: ImeAction = ImeAction.Done,
+    placeholder: String? = null,
+) {
+    Column {
+        Text(text = stringResource(id = text), textAlign = TextAlign.Start)
+        OutlinedTextField(
+            value = value,
+            onValueChange = onValueChange,
             singleLine = true,
             modifier = Modifier
                 .width(358.dp)
                 .height(82.dp)
                 .padding(horizontal = 16.dp, vertical = 12.dp),
-            placeholder = {placeholder?.let {
-                Text(text = placeholder)}
-                          },
+            placeholder = {
+                placeholder?.let {
+                    Text(text = placeholder)
+                }
+            },
             keyboardOptions = KeyboardOptions(
                 keyboardType = keyBoardType,
-                imeAction = imAction),
-            keyboardActions = KeyboardActions(onDone = { onDone})
-
+                imeAction = imAction
+            ),
+            keyboardActions = KeyboardActions(onDone = { onDone() })
         )
     }
 
 }
+
+
+@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES)
+@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_NO)
+@Composable
+fun TextFieldPreview() {
+    Column {
+        TextField(
+            leadingIcon = painterResource(id = R.drawable.heart),
+            value = "text",
+            onValueChange = { },
+            text = R.string.email,
+            placeholder = "Enter your email",
+            keyBoardType = KeyboardType.Text,
+            onDone = { /* handle done action */ },
+            modifier = Modifier.padding(16.dp)
+        )
+        TextFieldWithoutIcon(
+            value = "text",
+            onValueChange = { },
+            text = R.string.password,
+            placeholder = "Enter your password",
+            keyBoardType = KeyboardType.Password,
+            onDone = { /* handle done action */ },
+            modifier = Modifier.padding(16.dp)
+        )
+    }
+}
+
