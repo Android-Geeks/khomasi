ddf21e1609fb7a7ca64801133a67283fe187d656
Ali <alialashwall14@gmail.com>
2024-02-19 03:44:02 +0200

    comment Card

--- app/src/main/java/com/company/khomasi/presentation/MainActivity.kt
+++ app/src/main/java/com/company/khomasi/presentation/MainActivity.kt
@@ -21,7 +21,6 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     AppNavigation()
-
                 }
             }
         }
--- app/src/main/java/com/company/khomasi/presentation/components/Playground.kt
+++ app/src/main/java/com/company/khomasi/presentation/components/Playground.kt
@@ -19,3 +19,12 @@ data class BookingDetails(
     val playground: Playground,
     val statusOfBooking:String
 )
+
+data class CommentDetails(
+    val userName: String,
+    val userImageUrl : String,
+    val comment : String,
+    val date: String,
+    val time: String,
+
+)
