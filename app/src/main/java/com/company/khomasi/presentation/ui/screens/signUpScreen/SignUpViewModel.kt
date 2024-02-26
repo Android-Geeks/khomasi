1438ec197005584fa1832bd0691ca3ce0edc4687
Zeinab979 <zeinabwahdan3677@gmail.com>
2024-02-26 22:44:46 +0200

    add viewModel for signup screen

---
+++ app/src/main/java/com/company/khomasi/presentation/ui/screens/signUpScreen/SignUpViewModel.kt
@@ -0,0 +1,27 @@
+package com.company.khomasi.presentation.ui.screens.SignUpScreen
+
+import androidx.lifecycle.ViewModel
+
+class SignUpViewModel : ViewModel() {
+    //private val _uiState = MutableStateFlow(UiState())
+   // val uiState: StateFlow<UiState> = _uiState.asStateFlow()
+
+    fun createAccount(){
+
+    }
+
+    fun logIn(){
+
+    }
+
+    fun logo(){
+
+    }
+    fun privacyAndPolicy(){
+
+    }
+    fun helpAndSupport(){
+
+    }
+
+}
