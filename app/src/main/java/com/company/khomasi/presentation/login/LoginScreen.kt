5f3a195b7dbd40289c2cd22a98f22354a962e129
Ali <alialashwall14@gmail.com>
2024-03-15 01:02:24 +0200

    fix login keyboard

--- app/src/main/java/com/company/khomasi/presentation/login/LoginScreen.kt
+++ app/src/main/java/com/company/khomasi/presentation/login/LoginScreen.kt
@@ -5,13 +5,10 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.imePadding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.State
@@ -73,7 +70,7 @@ fun LoginScreen(
     Column(modifier = Modifier
         .fillMaxSize()
         .imePadding()
-        .verticalScroll(rememberScrollState())) {
+        ) {
         if (showLoading) {
             Loading()
         }
