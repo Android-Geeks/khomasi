d8c6e5a0858ace8aee28c8af154ca0f1a6406921
Zeyad Abdullah <zeyadgalal93@gmail.com>
2024-03-14 04:44:29 +0200

    add paging lib

--- app/src/main/java/com/company/khomasi/domain/model/UserLoginResponse.kt
+++ app/src/main/java/com/company/khomasi/domain/model/UserLoginResponse.kt
@@ -13,3 +13,31 @@ data class UserLoginResponse(
     @SerialName("user")
     val user: User
 )
+
+@Serializable
+data class User(
+    @SerialName("city")
+    val city: String,
+    @SerialName("coins")
+    val coins: Int,
+    @SerialName("country")
+    val country: String,
+    @SerialName("email")
+    val email: String,
+    @SerialName("firstName")
+    val firstName: String,
+    @SerialName("lastName")
+    val lastName: String,
+    @SerialName("latitude")
+    val latitude: Float,
+    @SerialName("longitude")
+    val longitude: Float,
+    @SerialName("phoneNumber")
+    val phoneNumber: String,
+    @SerialName("profilePicture")
+    val profilePicture: String?,
+    @SerialName("rating")
+    val rating: Int,
+    @SerialName("userID")
+    val userID: String
+)
