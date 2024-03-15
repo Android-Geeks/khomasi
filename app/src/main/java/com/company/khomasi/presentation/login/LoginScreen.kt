ecf05e680e48d1b34a9bb400c6266dabc48a5cef
Ali <alialashwall14@gmail.com>
2024-03-15 05:45:29 +0200

    fix login keyboard

--- app/src/main/java/com/company/khomasi/presentation/login/LoginScreen.kt
+++ app/src/main/java/com/company/khomasi/presentation/login/LoginScreen.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.State
@@ -100,7 +102,8 @@ fun LoginScreen(
                 loginWithGmail = loginWithGmail,
                 privacyAndPolicy = privacyAndPolicy,
                 helpAndSupport = helpAndSupport,
-                isValidEmailAndPassword = isValidEmailAndPassword
+                isValidEmailAndPassword = isValidEmailAndPassword,
+                modifier = modifier.verticalScroll(rememberScrollState())
             )
         }
     }
