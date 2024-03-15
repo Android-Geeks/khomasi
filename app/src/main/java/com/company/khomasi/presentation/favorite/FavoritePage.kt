46960f54d1b39c5e8cdbc3ac85b94dfca7b9a192
Zeinab979 <zeinabwahdan3677@gmail.com>
2024-03-15 21:30:05 +0200

    fav uiii

--- app/src/main/java/com/company/khomasi/presentation/favorite/FavoritePage.kt
+++ app/src/main/java/com/company/khomasi/presentation/favorite/FavoritePage.kt
@@ -1,15 +1,11 @@
 package com.company.khomasi.presentation.favorite
 
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
