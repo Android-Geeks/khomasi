e6f2b7a7cb506355d29b2f2bd603ed3c9493083f
Ali <alialashwall14@gmail.com>
2024-04-18 03:04:13 +0200

    add time slots fun to viewModel

--- app/src/main/java/com/company/khomasi/presentation/mainActivity/MainActivity.kt
+++ app/src/main/java/com/company/khomasi/presentation/mainActivity/MainActivity.kt
@@ -2,18 +2,16 @@ package com.company.khomasi.presentation.mainActivity
 
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.collectAsState
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
-import androidx.hilt.navigation.compose.hiltViewModel
-import com.company.khomasi.presentation.booking.BookingScreen
-import com.company.khomasi.presentation.booking.BookingViewModel
+import com.company.khomasi.presentation.navigation.NavGraph
 import com.company.khomasi.theme.KhomasiTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -32,14 +30,17 @@ class MainActivity : ComponentActivity() {
         setContent {
             KhomasiTheme {
                 Surface {
-//                    Log.d("TAG", mainViewModel.startDestination.value)
-//                    NavGraph(mainViewModel.startDestination.value)
-                    val bookingViewModel: BookingViewModel = hiltViewModel()
-                    BookingScreen(bookingUiState = bookingViewModel.bookingUiState.collectAsState().value,
-                        freeTimeState = bookingViewModel.freeSlotsState.collectAsState().value,
-                        updateDuration = { bookingViewModel.updateDuration(it) },
-                        getFreeSlots = { bookingViewModel.getFreeTimeSlots() }
-                    )
+                    Log.d("TAG", mainViewModel.startDestination.value)
+                    NavGraph(mainViewModel.startDestination.value)
+//                    val bookingViewModel: BookingViewModel = hiltViewModel()
+//                    BookingScreen(bookingUiState = bookingViewModel.bookingUiState.collectAsState().value,
+//                        freeTimeState = bookingViewModel.freeSlotsState.collectAsState().value,
+//                        onBackClicked = {},////////////////////////////////
+//                        updateDuration = { bookingViewModel.updateDuration(it) },
+//                        getFreeSlots = { bookingViewModel.getFreeTimeSlots() },
+//                        updateSelectedDay = { bookingViewModel.updateSelectedDay(it) },
+//                        onTimeSlotClicked = { bookingViewModel.onSlotClicked(it) }
+//                    )
                 }
             }
         }
