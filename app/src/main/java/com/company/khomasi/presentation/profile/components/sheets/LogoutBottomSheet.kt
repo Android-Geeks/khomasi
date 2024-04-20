f9dd3bc4cce7f49088020ea6849f2521101069cf
Zeyad Abdullah <zeyadgalal93@gmail.com>
2024-04-20 14:08:10 +0200

    fix logout bottom sheet

--- app/src/main/java/com/company/khomasi/presentation/profile/components/sheets/LogoutBottomSheet.kt
+++ app/src/main/java/com/company/khomasi/presentation/profile/components/sheets/LogoutBottomSheet.kt
@@ -5,10 +5,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -21,7 +21,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -45,17 +44,15 @@ fun LogoutBottomSheet(
     logout: () -> Unit,
     isDark: Boolean
 ) {
-    val sheetHeight = LocalConfiguration.current.screenHeightDp.dp / 2
-
     MyModalBottomSheet(
         sheetState = bottomSheetState,
         onDismissRequest = onDismissRequest,
         modifier = Modifier
-            .height(sheetHeight)
+            .wrapContentHeight()
     ) {
         Column(
             modifier = Modifier
-                .fillMaxSize()
+                .wrapContentHeight()
                 .padding(horizontal = 16.dp),
             verticalArrangement = Arrangement.spacedBy(16.dp),
             horizontalAlignment = Alignment.Start
@@ -86,7 +83,9 @@ fun LogoutBottomSheet(
                     color = MaterialTheme.colorScheme.tertiary
                 )
                 Spacer(modifier = Modifier.height(16.dp))
-                Row {
+                Row(
+                    modifier = Modifier.padding(bottom = 16.dp)
+                ) {
                     TextButton(
                         onClick = {
                             scope.launch {
