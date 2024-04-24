7da306ff7a01de3fb6c66e8314f1cccb5953c4d7
Ali <alialashwall14@gmail.com>
2024-04-24 05:56:41 +0200

    edit icon direction

--- app/src/main/java/com/company/khomasi/presentation/booking/BookingScreen.kt
+++ app/src/main/java/com/company/khomasi/presentation/booking/BookingScreen.kt
@@ -6,7 +6,6 @@ import android.os.Build
 import android.util.DisplayMetrics
 import android.widget.Toast
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
@@ -21,8 +20,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
@@ -34,7 +31,6 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -43,7 +39,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -166,7 +161,6 @@ fun BookingScreenContent(
     updateSelectedDay: (Int) -> Unit,
     onSlotClicked: (Pair<LocalDateTime, LocalDateTime>) -> Unit,
 ) {
-
     LaunchedEffect(bookingUiState.selectedDay) {
         getFreeSlots()
     }
@@ -320,80 +314,6 @@ fun calculateHourlyIntervalsList(
         emptyList()
     }
 }
-
-@Composable
-fun SlotItem(
-    slotStart: String,
-    slotEnd: String,
-    isSelected: MutableState<Boolean>,
-    onClickSlot: () -> Unit = {}
-) {
-    val cardColor = if (isSelected.value) MaterialTheme.colorScheme.primary else Color.Transparent
-    val textColor =
-        if (isSelected.value) MaterialTheme.colorScheme.background else MaterialTheme.colorScheme.primary
-    val textSize =
-        if (isSelected.value) MaterialTheme.typography.titleLarge else MaterialTheme.typography.titleMedium
-    val currentLanguage = Locale.getDefault().language
-    Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(52.dp)
-            .border(
-                color = MaterialTheme.colorScheme.primary,
-                width = 1.dp,
-                shape = MaterialTheme.shapes.medium
-            )
-            .clickable {
-                onClickSlot()
-                isSelected.value = !isSelected.value
-            },
-        shape = MaterialTheme.shapes.medium,
-        colors = CardDefaults.cardColors(cardColor),
-    ) {
-        Row(
-            modifier = Modifier.fillMaxSize(),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Center
-        ) {
-            Text(
-                text = slotStart,
-                color = textColor,
-                style = textSize,
-
-                )
-            Spacer(modifier = Modifier.width(4.dp))
-            if (isSelected.value) {
-                Icon(
-                    painter = painterResource(id = R.drawable.arrow_left),
-                    tint = MaterialTheme.colorScheme.background,
-                    modifier = Modifier
-                        .size(24.dp)
-                        .then(
-                            if (currentLanguage == "en") {
-                                Modifier.rotate(180f)
-                            } else {
-                                Modifier
-                            }
-                        ),
-                    contentDescription = null,
-                )
-            } else {
-                Text(
-                    text = "_",
-                    color = textColor,
-                    style = textSize,
-                )
-            }
-            Spacer(modifier = Modifier.width(4.dp))
-            Text(
-                text = slotEnd,
-                color = textColor,
-                style = textSize,
-            )
-        }
-    }
-}
-
 @RequiresApi(Build.VERSION_CODES.O)
 fun formatTime(localDateTime: LocalDateTime): String {
     return localDateTime.format(DateTimeFormatter.ofPattern("hh:mm a"))
@@ -423,6 +343,7 @@ fun BookingTopBar(
     playgroundName: String, onBackClicked: () -> Unit = {},
     context: Context
 ) {
+    val currentLanguage = Locale.getDefault().language
     Column {
         Row(
             modifier = Modifier.fillMaxWidth(),
@@ -446,7 +367,15 @@ fun BookingTopBar(
                     contentDescription = null,
                     Modifier
                         .size(24.dp)
-                        .clickable { onBackClicked() })
+                        .clickable { onBackClicked() }
+                        .then(
+                            if (currentLanguage == "en") {
+                                Modifier.rotate(180f)
+                            } else {
+                                Modifier
+                            }
+                        )
+                )
             }, colors = TopAppBarDefaults.topAppBarColors(MaterialTheme.colorScheme.background)
             )
         }
