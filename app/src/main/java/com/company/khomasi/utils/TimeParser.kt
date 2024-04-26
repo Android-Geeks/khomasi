aae5d211676513fb0580cc72694860ecf5442fc5
Ali <alialashwall14@gmail.com>
2024-04-26 03:58:43 +0300

    add date parsing

--- app/src/main/java/com/company/khomasi/utils/TimeParser.kt
+++ app/src/main/java/com/company/khomasi/utils/TimeParser.kt
@@ -7,11 +7,6 @@ import java.time.OffsetDateTime
 import java.time.format.DateTimeFormatter
 
 @RequiresApi(Build.VERSION_CODES.O)
-fun formatTime(localDateTime: LocalDateTime): String {
-    return localDateTime.format(DateTimeFormatter.ofPattern("hh:mm a"))
-}
-
-@RequiresApi(Build.VERSION_CODES.O)
 fun parseTimestamp(timestamp: String): LocalDateTime {
     return try {
         val offsetDateTime = OffsetDateTime.parse(timestamp)
@@ -20,4 +15,14 @@ fun parseTimestamp(timestamp: String): LocalDateTime {
         // If parsing fails, assume timestamp is in UTC time
         LocalDateTime.parse(timestamp)
     }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+fun extractTimeFromTimestamp(localDateTime: LocalDateTime): String {
+    return localDateTime.format(DateTimeFormatter.ofPattern("hh:mm a"))
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+fun extractDateFromTimestamp(localDateTime: LocalDateTime): String {
+    return localDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
 }
