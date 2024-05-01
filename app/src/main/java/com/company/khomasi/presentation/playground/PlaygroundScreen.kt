e48ee3c7c1adb642b9c9baf2182056d38d7b500f
Zeyad Abdullah <zeyadgalal93@gmail.com>
2024-05-01 19:33:54 +0300

    fix size

--- app/src/main/java/com/company/khomasi/presentation/playground/PlaygroundScreen.kt
+++ app/src/main/java/com/company/khomasi/presentation/playground/PlaygroundScreen.kt
@@ -38,7 +38,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
@@ -87,7 +86,6 @@ fun PlaygroundScreen(
     updateShowReview: () -> Unit,
 ) {
     val showLoading by remember { mutableStateOf(false) }
-//    val uiState by playgroundUiState.collectAsStateWithLifecycle()
     val reviews by reviewsState.collectAsStateWithLifecycle()
     val bottomSheetState = rememberModalBottomSheetState()
     val scope = rememberCoroutineScope()
@@ -244,7 +242,7 @@ fun PlaygroundScreenContent(
 
         item { LineSpacer() }
 
-        item { PlaygroundSize(size = stringResource(R.string.field_size_5x5)) }
+        item { PlaygroundSize(playgroundStateFlow = playgroundStateFlow) }
 
         item { LineSpacer() }
 
--- app/src/main/java/com/company/khomasi/presentation/playground/components/PlaygroundSize.kt
+++ app/src/main/java/com/company/khomasi/presentation/playground/components/PlaygroundSize.kt
@@ -14,17 +14,30 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.company.khomasi.R
+import com.company.khomasi.domain.DataState
+import com.company.khomasi.domain.model.PlaygroundScreenResponse
+import kotlinx.coroutines.flow.StateFlow
 
 @Composable
 fun PlaygroundSize(
-    size: String
+    playgroundStateFlow: StateFlow<DataState<PlaygroundScreenResponse>>,
 ) {
+    val playgroundState by playgroundStateFlow.collectAsStateWithLifecycle()
+    var playgroundData by remember { mutableStateOf(5) }
+    if (playgroundState is DataState.Success) {
+        playgroundData = (playgroundState as DataState.Success).data.playground.type
+    }
     Box {
         Column(
             Modifier
@@ -53,7 +66,7 @@ fun PlaygroundSize(
                     horizontalArrangement = Arrangement.Center
                 ) {
                     Text(
-                        text = size,
+                        text = "$playgroundData X $playgroundData",
                         style = MaterialTheme.typography.displayMedium,
                         color = MaterialTheme.colorScheme.tertiary
                     )
