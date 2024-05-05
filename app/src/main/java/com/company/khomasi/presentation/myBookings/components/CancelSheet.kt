e8283c957da79ea04cae75c55f17ee25b2d79c08
Zeyad Abdullah <zeyadgalal93@gmail.com>
2024-05-05 18:50:57 +0300

    fix material import with material3

--- app/src/main/java/com/company/khomasi/presentation/myBookings/components/CancelSheet.kt
+++ app/src/main/java/com/company/khomasi/presentation/myBookings/components/CancelSheet.kt
@@ -11,12 +11,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material.Scaffold
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
