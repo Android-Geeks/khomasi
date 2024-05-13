63cfac7f3cb0d4b35e6683194bbdedc42bc11e9c
Zeyad Abdullah <zeyadgalal93@gmail.com>
2024-05-13 10:43:45 +0300

    add hide system ui

--- app/src/main/java/com/company/khomasi/presentation/mainActivity/MainActivity.kt
+++ app/src/main/java/com/company/khomasi/presentation/mainActivity/MainActivity.kt
@@ -45,5 +45,23 @@ class MainActivity : ComponentActivity() {
                 )
             }
         }
+        //hideSystemUI()
     }
+
+//    private fun hideSystemUI() {
+//        //Hides the ugly action bar at the top
+//        actionBar?.hide()
+//        //Hide the status bars
+//        WindowCompat.setDecorFitsSystemWindows(window, false)
+//        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.R) {
+//            window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+//        } else {
+//            window.insetsController?.apply {
+//                hide(WindowInsets.Type.statusBars())
+//                systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+//            }
+//        }
+//    }
 }
+
+
