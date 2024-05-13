22f8fb6a6d8780c0dcac4f767394e1dc2fa3eead
Zeyad Abdullah <zeyadgalal93@gmail.com>
2024-05-13 07:51:10 +0300

    fix modal bottom sheet

--- app/src/main/java/com/company/khomasi/presentation/components/MyModalBottomSheet.kt
+++ app/src/main/java/com/company/khomasi/presentation/components/MyModalBottomSheet.kt
@@ -1,6 +1,8 @@
 package com.company.khomasi.presentation.components
 
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.ime
 import androidx.compose.material3.BottomSheetDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
@@ -23,6 +25,7 @@ fun MyModalBottomSheet(
         sheetState = sheetState,
         dragHandle = { BottomSheetDefaults.DragHandle() },
         modifier = modifier,
+        windowInsets = WindowInsets.ime,
         containerColor = if (isSystemInDarkTheme()) darkOverlay else lightOverlay
     ) {
         content()
