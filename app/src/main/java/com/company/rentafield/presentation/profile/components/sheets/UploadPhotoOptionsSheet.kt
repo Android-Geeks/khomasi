3ff7203b1ff32257f3669865d09e751f22fae61c
Zeyad Abdullah <zeyadgalal93@gmail.com>
2024-06-17 16:58:57 +0300

    fix upload image options ui

--- app/src/main/java/com/company/rentafield/presentation/profile/components/sheets/UploadPhotoOptionsSheet.kt
+++ app/src/main/java/com/company/rentafield/presentation/profile/components/sheets/UploadPhotoOptionsSheet.kt
@@ -1,20 +1,20 @@
 package com.company.rentafield.presentation.profile.components.sheets
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SheetState
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.company.rentafield.R
+import com.company.rentafield.presentation.components.MyButton
 import com.company.rentafield.presentation.components.MyModalBottomSheet
+import com.company.rentafield.presentation.components.MyOutlinedButton
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -30,36 +30,30 @@ fun UploadPhotoOptionsSheet(
         modifier = Modifier
             .wrapContentHeight()
     ) {
-        Row(
-            modifier = Modifier.padding(16.dp),
-            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
         ) {
-            Button(
+            MyOutlinedButton(
+                text = R.string.take_a_photo,
                 onClick = {
                     onDismissRequest()
                     onTakePhoto()
                 },
                 shape = MaterialTheme.shapes.medium,
-                modifier = Modifier.weight(1f)
-            ) {
-                Text(
-                    text = stringResource(id = R.string.take_a_photo),
-                    style = MaterialTheme.typography.titleLarge,
-                )
-            }
-            Button(
+                modifier = Modifier.fillMaxWidth()
+            )
+            MyButton(
+                text = R.string.choose_from_gallery,
                 onClick = {
                     onDismissRequest()
                     onChooseFromGallery()
                 },
                 shape = MaterialTheme.shapes.medium,
-                modifier = Modifier.weight(1f)
-            ) {
-                Text(
-                    text = stringResource(id = R.string.choose_from_gallery),
-                    style = MaterialTheme.typography.titleLarge,
-                )
-            }
+                modifier = Modifier.fillMaxWidth()
+            )
         }
     }
 }
