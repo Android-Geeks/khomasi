fdaed881af2bfd981cd844551d8c1af2c5c14dc1
menna <94606987+Menna120@users.noreply.github.com>
2024-09-30 20:34:24 +0300

    refactor favourite screen into MVI

---
+++ app/src/main/java/com/company/rentafield/presentation/screens/favorite/EmptyFavouriteScreen.kt
@@ -0,0 +1,43 @@
+package com.company.rentafield.presentation.screens.favorite
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.company.rentafield.R
+
+@Composable
+fun EmptyScreen(
+    modifier: Modifier = Modifier,
+    isDark: Boolean = isSystemInDarkTheme()
+) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(
+                if (isDark) R.drawable.dark_fav_screen
+                else R.drawable.light_empty_fav_screen
+            ),
+            contentDescription = null,
+            alignment = Alignment.Center
+        )
+        Text(
+            text = stringResource(R.string.no_favorite_fields),
+            modifier = Modifier.padding(16.dp),
+            style = MaterialTheme.typography.titleSmall
+        )
+    }
+}
